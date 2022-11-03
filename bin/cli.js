fdd6c8179eaf0e1fea3756a15f5a100527a330e7
Kush Shah <kush@unc.edu>
2022-11-03 17:49:15 -0400

    exportable roll function and cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+// import statements
+import {roll} from '../lib/roll.js';
+import minimist from 'minimist';
+
+// parse command line arguments
+const args = minimist(process.argv.slice(2))
+
+return(roll(args.sides, args.dice, args.rolls));
