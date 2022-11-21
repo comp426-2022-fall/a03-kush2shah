f4eef0912135d91cd6616d76dc9e566da5bfd948
Kush Shah <kush@unc.edu>
2022-11-20 23:03:37 -0500

    executable, finally

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // import statements
-import {roll} from "lib/roll.js";
 import minimist from 'minimist';
+import {roll} from "../lib/roll.js";
 
 // parse command line arguments
 const args = minimist(process.argv.slice(2))
