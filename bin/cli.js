7f74453f0d770a75f6ee396e767266d63b57cc05
Kush Shah <kush@unc.edu>
2022-11-20 22:51:38 -0500

    changes from lecture

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
 // import statements
-import {roll} from '../lib/roll.js';
+import {roll} from "lib/roll.js";
 import minimist from 'minimist';
 
 // parse command line arguments
 const args = minimist(process.argv.slice(2))
 
-return(roll(args.sides, args.dice, args.rolls));
+let sides = args.sides;
+let dice = args.dice;
+let rolls = args.rolls;
+if(sides < 0 || dice < 0 || rolls < 0){
+    console.log("Please enter positive numbers");
+    process.exit(1);
+}
+let returnedObject = roll(sides, dice, rolls);
+console.log(returnedObject);
+process.exit(0);
